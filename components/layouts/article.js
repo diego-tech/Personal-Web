53d0d96f63841f7f67ce96ad82e4329ab1987458
Diego Muñoz Herranz <diego171200@gmail.com>
2023-10-01 11:32:15 +0200

    Fix Hydrate Error

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -3,28 +3,30 @@ import Head from 'next/head'
 import { GridItemsStyle } from "../grid-item"
 
 const variants = {
-    hidden: { opacity: 0, x: 0, y: 20},
-    enter: { opacity: 1, x: 0, y: 0},
-    exit: { opacity:0, x: 0, y: 20}
+    hidden: { opacity: 0, x: 0, y: 20 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: 20 }
 }
 
-const Layout = ({ children, title }) => (
-    <motion.article
+const Layout = ({ children, title }) => {
+    const t = `${title} - Diego Muñoz Herranz`
+    return (<motion.article
         initial="hidden"
         animate="enter"
         exit="exit"
         variants={variants}
-        transition={{duration: 0.4, type: 'easeInOut'}}
-        style={{position: 'relative'}}>
+        transition={{ duration: 0.4, type: 'easeInOut' }}
+        style={{ position: 'relative' }}>
         <>
-        {title && ( <Head>
-            <title>{title} - Diego Muñoz Herranz</title>
-        </Head>
-        )}
-        {children}
-        <GridItemsStyle />
-        </>    
+            {title && (<Head>
+                <title>{t}</title>
+            </Head>
+            )}
+            {children}
+            <GridItemsStyle />
+        </>
     </motion.article>
-)
+    )
+}
 
 export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -13,10 +13,14 @@ const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
             <GlobalStyles />
-            <Fonts/>
+            <Fonts />
             <Layout router={router}>
-                <AnimatePresence initial={true}>
-                    <Component {...pageProps} key={router.route}/>
+                <AnimatePresence mode="wait" initial={true} onExitComplete={() => {
+                    if (typeof window !== 'undefined') {
+                        window.scrollTo({ top: 0 })
+                    }
+                }}>
+                    <Component {...pageProps} key={router.route} />
                 </AnimatePresence>
             </Layout>
         </ChakraProvider>
